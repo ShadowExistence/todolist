6f53676b41ab0b68cdfccd01666dedbbf519c48e
Szymon Dizewski <szymon.dizewski1@gmail.com>
2022-09-22 21:39:54 +0200

    finito

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import {ref, computed} from 'vue'
+
+
+  var list = ref([])
+  var string = ref()
+  function add(){
+    list.value.push(string.value)
+    console.log(`added ${string.value}`)
+  }
+
+  function del(i){
+    list.value.splice(i,1);
+  }
+
+  
+
+</script>
+
+
+<template >
+ <ul>
+  <li  v-for="(q,i) in list" :key="i" @click="del(i)">{{q}}</li>
+ </ul>
+ <input @keydown.enter="add" v-model="string" type="text" />
+ <button  @click="add">Add</button>
+</template>
+
